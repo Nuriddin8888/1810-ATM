67d000c60cb036763bddb701ed6f3e0cf1e204c6
Your Name <mutalovnuriddin651@gmail.com>
2025-07-23 09:17:02 +0500

    Chenge password success

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ if user:
         2.Pul Yechish
         3.Pul o'tkazish
         4.Parolni o'zgartirish
-        5.Chiqish
+        5.Hisobni almashtirish
+        0.Chiqish
 """)
             
             user_choose = input("Bo'limni tanlang: ")
@@ -39,3 +40,28 @@ if user:
                     withdraw_money = user['balance'] - user_money
                     user["balance"] = withdraw_money
                     print(f"Hisobingizda qolgan summa {user['balance']}")
+
+            elif user_choose == "3":
+                send_card = int(input("Pul o'tkazmoqchi bo'lgan karta raqamini kiriting: "))
+                if send_card in users:
+                    send_money = int(input("Summani kiriting: "))
+                    if send_money <= user['balance']:
+                        new_money = user["balance"] - send_money
+                        user["balance"] = new_money
+                        print("Pul muaffaqqiyatli amalga oshirildi!")
+                    else:
+                        print("Sizing hisobingizda yetarlicha mablag' yo'q")
+                else:
+                    print("Bunday karta mavjud emas!")
+
+            elif user_choose == "4":
+                old_password = input("Oldingi parolni kiritng: ")
+                new_password = input("Yangi parol kiriting: ")
+                reset_password = input("Yana takroran kiritng: ")
+                cheng_password(user, old_password, new_password, reset_password)
+                user_new_pass = input("Parol kiriting: ")
+                if user_new_pass == user["password"]:
+                    continue
+                else:
+                    print("Parol noto'g'ri bank bilan bog'laning (+99877 777-77-78)")
+                    break
--- servise.py
+++ servise.py
@@ -16,3 +16,14 @@ def show_balance(user):
 
 def withdraw(user):
     return user['balance']
+
+
+def cheng_password(user, old_password, new_password, reset_password):
+    if old_password == user["password"]:
+        if new_password == reset_password:
+            user["password"] = new_password
+            print("Parolingiz muaffaqqiyatli o'zgartirildi")
+        else:
+            print("Parollar mos emas")
+    else:
+        print("Parol noto'g'ri")
